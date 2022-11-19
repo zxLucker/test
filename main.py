fe52ea5b3813a2defacf953986726118ac8c73f9
thiscofingroyou@gmail.com <153486As@>
2022-11-19 08:04:50 +0300

    Добавили файл src.py

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def change_value(change):
+    return change
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    a = change_value(5)
+    print(a)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
